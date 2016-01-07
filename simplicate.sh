468c0a30606f06d76a34f12bcfc16ab0b858766f
Nick Cartwright <nick@aslant.io>
2016-01-07 17:46:30 +0000

    Remove unnecessary semicolons

--- simplicate.sh
+++ simplicate.sh
@@ -1,108 +1,123 @@
 #!/bin/bash
 
-OPTIND=1;
-continuous=0;
-verbose=0;
-bad_opt=0;
+OPTIND=1
+continuous=0
+verbose=0
+bad_opt=0
 
-while getopts ":s:t:d:f:q:cont:v" o; do
+while getopts ":s:t:d:f:q:cont:v" o
+do
   case "${o}" in
     s)
-      src=${OPTARG};
-      if [[ $src =~ ^:[0-9]+/ ]]; then
+      src=${OPTARG}
+      if [[ $src =~ ^:[0-9]+/ ]]
+      then
         src=http://127.0.0.1$src
       fi
       ;;
     t)
-      tar=${OPTARG};
-      if [[ $tar =~ ^:[0-9]+/ ]]; then
+      tar=${OPTARG}
+      if [[ $tar =~ ^:[0-9]+/ ]]
+      then
         tar=http://127.0.0.1$tar
       fi
       ;;
     d)
-      doc_ids=${OPTARG};
+      doc_ids=${OPTARG}
       ;;
     f)
-      filter=${OPTARG};
+      filter=${OPTARG}
       ;;
     q)
-      query_params=${OPTARG};
+      query_params=${OPTARG}
       ;;
     c)
-      continuous=1;
+      continuous=1
       ;;
     v)
-      verbose=1;
+      verbose=1
       ;;
     *)
-      bad_opt=1;
-      break;
+      bad_opt=1
+      break
       ;;
   esac
 done
 
-if [ $bad_opt -eq 0 ]; then
-  shift $((OPTIND-1));
-  url=$1;
+if [ $bad_opt -eq 0 ]
+then
+  shift $((OPTIND-1))
+  url=$1
   OPTIND=1; # cleanup
 fi
 
-if [ -z "$url" ]; then
-  url="http://127.0.0.1:5984";
-elif [[ $url =~ ^:[0-9]+ ]]; then
+if [ -z "$url" ]
+then
+  url="http://127.0.0.1:5984"
+elif [[ $url =~ ^:[0-9]+ ]]
+then
   url=http://127.0.0.1$url
 fi
 
-if [ -z "$src" ] && [ -z "$tar" ] || [ -z "$url" ]; then
+if [ -z "$src" ] && [ -z "$tar" ] || [ -z "$url" ]
+then
   OPTIND=1; # cleanup
 
-  symlinktarget="$(readlink "$0")";
-  cd "$(dirname "$0")";
+  symlinktarget="$(readlink "$0")"
+  cd "$(dirname "$0")"
 
-  if [ -n "$symlinktarget" ]; then
-    cd "$(dirname "$symlinktarget")";
+  if [ -n "$symlinktarget" ]
+  then
+    cd "$(dirname "$symlinktarget")"
   fi
 
   cat usage.txt >&2
-  exit 1;
+  exit 1
 fi
 
-if [ -z "$tar" ]; then
-  tar=$(echo "$src" | perl -pe "s|.*?([^/]*)/?$|\1|");
+if [ -z "$tar" ]
+then
+  tar=$(echo "$src" | perl -pe "s|.*?([^/]*)/?$|\1|")
 fi
 
-if [ -z "$src" ]; then
-  src=$(echo "$tar" | perl -pe "s|.*?([^/]*)/?$|\1|");
+if [ -z "$src" ]
+then
+  src=$(echo "$tar" | perl -pe "s|.*?([^/]*)/?$|\1|")
 fi
 
 body="{\"create_target\":true",\"source\":\"${src}\",\"target\":\"${tar}\"
 
-if [ -n "$doc_ids" ]; then
-  body="${body},\"doc_ids\":${doc_ids}";
+if [ -n "$doc_ids" ]
+then
+  body="${body},\"doc_ids\":${doc_ids}"
 fi
 
-if [ -n "$filter" ]; then
-  body="${body},\"filter\":\"${filter}\"";
+if [ -n "$filter" ]
+then
+  body="${body},\"filter\":\"${filter}\""
 fi
 
-if [ -n "$query_params" ]; then
-  body="${body},\"query_params\":${query_params}";
+if [ -n "$query_params" ]
+then
+  body="${body},\"query_params\":${query_params}"
 fi
 
-if [ "$continuous" -eq 1 ]; then
-  body="${body},\"continuous\":true";
+if [ "$continuous" -eq 1 ]
+then
+  body="${body},\"continuous\":true"
 fi
 
-body="${body}}";
+body="${body}}"
 
 cmd="curl" && [ $verbose -eq 1 ] && cmd="curl -v"
 cmd="${cmd} -H 'Content-Type: application/json' -X POST ${url}/_replicate -d '${body}'"
-echo;
-echo "  $cmd";
-echo;
+echo
+echo "  $cmd"
+echo
 
 read -r -p "execute command? [y/N]" response
-if [[ $response =~ ^[yY]$ ]]; then
+if [[ $response =~ ^[yY]$ ]]
+then
   echo `eval ${cmd}`
 else
   exit 1
